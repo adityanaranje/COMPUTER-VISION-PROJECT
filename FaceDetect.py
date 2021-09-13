8601c8cb6f916cd5b54c8d912a850f21f829f26d
Aditya Naranje <75174363+adityanaranje@users.noreply.github.com>
2021-09-13 12:49:10 +0530

    Update FaceDetect.py

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,37 +1,7 @@
 import cv2
-from imutils.video import WebcamVideoStream
+
 
 faceDetect = cv2.CascadeClassifier('data/haarcascade_facedetect.xml')
-
-
-# Face detection from webcam
-class webcameraface(object):
-    def __init__(self):
-        self.video = WebcamVideoStream(src=0).start()
-
-    def __del__(self):
-        self.video.stop()
-
-    def get_frame1(self):
-        ret, frame = self.video.read()
-        faces = faceDetect.detectMultiScale(frame, 1.1, 4)
-        for x, y, w, h in faces:
-            x1, y1 = x+w, y+h
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 255, 0), 1)
-            cv2.line(frame, (x, y), (x+25, y), (127, 255, 0), 5)  # Top Left
-            cv2.line(frame, (x, y), (x, y+25), (127, 255, 0), 5)
-
-            cv2.line(frame, (x, y1), (x+25, y1), (127, 255, 0), 5)  # Bottom Left
-            cv2.line(frame, (x, y1), (x, y1-25), (127, 255, 0), 5)
-
-            cv2.line(frame, (x1, y), (x1-25, y), (127, 255, 0), 5)  # Top Right
-            cv2.line(frame, (x1, y), (x1, y+25), (127, 255, 0), 5)
-
-            cv2.line(frame, (x1, y1), (x1-25, y1), (127, 255, 0), 5)  # Bottom right
-            cv2.line(frame, (x1, y1), (x1, y1-25), (127, 255, 0), 5)
-
-        ret, jpg = cv2.imencode('.jpg', frame)
-        return jpg.tobytes()
 
 
 # Face detection from video
