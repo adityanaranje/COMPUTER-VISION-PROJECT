ef61d9bc16d45edea4c155dbff01d268107232a4
Aditya Naranje <75174363+adityanaranje@users.noreply.github.com>
2021-09-13 14:16:33 +0530

    Add files via upload

---
+++ FaceDetect.py
@@ -0,0 +1,58 @@
+import cv2
+
+faceDetect = cv2.CascadeClassifier('data/haarcascade_facedetect.xml')
+
+
+# Face detection from video
+class videofaces(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture("uploads/video1.mp4")
+
+    def __del__(self):
+        self.video.release()
+
+    def get_video1(self):
+        ret, frame = self.video.read()
+        faces = faceDetect.detectMultiScale(frame, 1.3, 5)
+        for x, y, w, h in faces:
+            x1, y1 = x+w, y+h
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (127, 255, 0), 1)
+            cv2.line(frame, (x, y), (x+25, y), (127, 255, 0), 5)  # Top Left
+            cv2.line(frame, (x, y), (x, y+25), (127, 255, 0), 5)
+
+            cv2.line(frame, (x, y1), (x+25, y1), (127, 255, 0), 5)  # Bottom Left
+            cv2.line(frame, (x, y1), (x, y1-25), (127, 255, 0), 5)
+
+            cv2.line(frame, (x1, y), (x1-25, y), (127, 255, 0), 5)  # Top Right
+            cv2.line(frame, (x1, y), (x1, y+25), (127, 255, 0), 5)
+
+            cv2.line(frame, (x1, y1), (x1-25, y1), (127, 255, 0), 5)  # Bottom right
+            cv2.line(frame, (x1, y1), (x1, y1-25), (127, 255, 0), 5)
+
+        ret, jpg = cv2.imencode('.jpg', frame)
+        return jpg.tobytes()
+
+
+# Face detection from image
+class imagefaces():
+    def get_image1(self):
+        image = cv2.imread("uploads/img1.jpg")
+
+        faces = faceDetect.detectMultiScale(image, 1.3, 5)
+        for x, y, w, h in faces:
+            x1, y1 = x+w, y+h
+            cv2.rectangle(image, (x, y), (x+w, y+h), (13, 187, 77), 1)
+            cv2.line(image, (x, y), (x+20, y), (13, 187, 77), 3)  # Top Left
+            cv2.line(image, (x, y), (x, y+20), (13, 187, 77), 3)
+
+            cv2.line(image, (x, y1), (x+20, y1), (13, 187, 77), 3)  # Bottom Left
+            cv2.line(image, (x, y1), (x, y1-20), (13, 187, 77), 3)
+
+            cv2.line(image, (x1, y), (x1-20, y), (13, 187, 77), 3)  # Top Right
+            cv2.line(image, (x1, y), (x1, y+20), (13, 187, 77), 3)
+
+            cv2.line(image, (x1, y1), (x1-20, y1), (13, 187, 77), 3)  # Bottom right
+            cv2.line(image, (x1, y1), (x1, y1-20), (13, 187, 77), 3)
+
+        ret, jpg = cv2.imencode('.jpg', image)
+        return jpg.tobytes()
---
+++ ObjDetect.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+thres = 0.5  # Threshold to detect object
+nms_threshold = 0.2
+
+
+classNames = []
+classFile = "data/coco.names"
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath1 = "data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath1 = "data/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath1, configPath1)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+COLORS = np.random.randint(0, 255, size=(len(classNames), 3), dtype="uint8")
+
+# Object detection from webcam
+
+
+# Object detection from Video
+
+
+class videoobjects(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture("uploads/video2.mp4")
+
+    def __del__(self):
+        self.video.release()
+
+    def get_video2(self):
+        success, img = self.video.read()
+
+        if img.shape[1] > 1500 and img.shape[1] < 2000 or img.shape[0] > 1500 and img.shape[0] < 2000:
+            img = cv2.resize(img, (int(img.shape[1]*0.55), int(img.shape[0]*0.55)))
+        if img.shape[1] > 2000 and img.shape[1] < 3000 or img.shape[0] > 2000 and img.shape[0] < 3000:
+            img = cv2.resize(img, (int(img.shape[1]*0.4), int(img.shape[0]*0.4)))
+        if img.shape[1] > 3000 or img.shape[0] > 3000:
+            img = cv2.resize(img, (int(img.shape[1]*0.2), int(img.shape[0]*0.2)))
+
+        classIds, confs, bbox = net.detect(img, confThreshold=thres)
+        bbox = list(bbox)
+        confs = list(np.array(confs).reshape(1, -1)[0])
+        confs = list(map(float, confs))
+        # print(type(confs[0]))
+        # print(confs)
+
+        indices = cv2.dnn.NMSBoxes(bbox, confs, thres, nms_threshold)
+        # print(indices)
+
+        for i in indices:
+            i = i[0]
+            box = bbox[i]
+            color = [int(c) for c in COLORS[classIds[i][0]-1]]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x, y), (x+w, h+y), color=color, thickness=2)
+            cv2.putText(img, classNames[classIds[i][0]-1].upper(), (box[0]+10, box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+
+        ret, jpg = cv2.imencode('.jpg', img)
+        return jpg.tobytes()
+
+
+# Object detection from Image
+
+
+class imageobjects():
+    def get_image2(self):
+        img = cv2.imread("uploads/image2.jpg")
+        classIds, confs, bbox = net.detect(img, confThreshold=thres)
+        bbox = list(bbox)
+        confs = list(np.array(confs).reshape(1, -1)[0])
+        confs = list(map(float, confs))
+        # print(type(confs[0]))
+        # print(confs)
+
+        indices = cv2.dnn.NMSBoxes(bbox, confs, thres, nms_threshold)
+        # print(indices)
+
+        for i in indices:
+            i = i[0]
+            box = bbox[i]
+            color = [int(c) for c in COLORS[classIds[i][0]-1]]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x, y), (x+w, h+y), color=color, thickness=2)
+            cv2.putText(img, classNames[classIds[i][0]-1].upper(), (box[0]+10, box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
+
+        ret, jpg = cv2.imencode('.jpg', img)
+        return jpg.tobytes()
